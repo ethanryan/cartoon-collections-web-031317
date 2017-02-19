5d646dafcb081bf0eb1f52da35dfddef19e513e0
Ethan Ryan <ethanryan@gmail.com>
2017-02-19 21:57:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  #puts dwarves #this doesn't work, can't see array of elements
+  array.each_with_index { |val,index| puts "#{index+1}. #{val}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  array.map { |word| puts "#{word.capitalize}!" } #map over each word, capitalize and add exclamation mark, (outputting to see results)
+  array.map { |word| "#{word.capitalize}!" } #map over each word, capitalize and add exclamation mark (can't say "return" before "#{word.capitalize}!")
 end
+
+
+##contrinue working on here:::::
 
 def long_planeteer_calls# code an argument here
   # Your code here
