57095282047bd082d244e1274d3752692305556d
Ethan Ryan <ethanryan@gmail.com>
2017-02-20 20:51:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,18 +4,26 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  array.map { |word| puts "#{word.capitalize}!" } #map over each word, capitalize and add exclamation mark, (outputting to see results)
+  #array.map { |word| puts "#{word.capitalize}!" } #map over each word, capitalize and add exclamation mark, (outputting to see results)
   array.map { |word| "#{word.capitalize}!" } #map over each word, capitalize and add exclamation mark (can't say "return" before "#{word.capitalize}!")
 end
 
+def long_planeteer_calls(array)# code an argument here
+  array.any? do |word|
+    word.length > 4
+  end #end do loop
+end #end method
 
-##contrinue working on here:::::
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# The find method returns the first element for which block is not false:
+# > arr.find {|a| a > 5} # returns the first element greater than 5 and `nil` if none satisfies the condition
+# => 6
+#note: find and detect are the same method, just different names
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  array.find {| cheese | cheese == cheese_types[0] }
+  #the above line searches the array for the argument 'cheese' (a local variable named 'cheese' within the pipes)
+  #and returns the first element within the array that == an element from cheese_types (located at index [0]),
+  #or returns nil if it can't find any elements within the array that == an element from cheese_types
+end #end method
